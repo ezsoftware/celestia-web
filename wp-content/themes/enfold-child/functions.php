ce8c7a8e013b2e5fa2ea8c388aea31ceee0d24e8
ezsoftware <john@ezsoftwareinc.com>
2017-05-27 11:09:19 -0700

    fix instance class name

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -7,7 +7,7 @@ class CW_Functions {
   static $instance = null;
   public static function getInstance() {
     if(self::$instance === null) {
-      self::$instance = new CW_Skeleton();
+      self::$instance = new CW_Functions();
     }
     return self::$instance;
   }
