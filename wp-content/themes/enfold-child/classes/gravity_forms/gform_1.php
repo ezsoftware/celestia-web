fb6319a83bb2fcc1d553801eed10f4414e759b9e
ezsoftware <john@ezsoftwareinc.com>
2017-05-27 15:24:24 -0700

    fix custom validation on user form

--- wp-content/themes/enfold-child/classes/gravity_forms/gform_1.php
+++ wp-content/themes/enfold-child/classes/gravity_forms/gform_1.php
@@ -14,8 +14,7 @@ class CW_GForm_1 {
   }
 
   public function form_1_4_validation($result, $value, $form, $field) {
-    var_dump($value);
-    if(!preg_match("[^A-Za-z0-9\s_\-\']", $value)) {
+    if(!preg_match("[^A-Za-z0-9\s_\-\']", $value['4.3']) && !preg_match("[^A-Za-z0-9\s_\-\']", $value['4.6'])) {
       $result['is_valid'] = true;
     } else {
       $result['is_valid'] = false;
