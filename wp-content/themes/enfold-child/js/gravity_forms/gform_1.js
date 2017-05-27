fbd434fd1c0e658b2c724e584e640b049039a604
ezsoftware <john@ezsoftwareinc.com>
2017-05-27 11:34:31 -0700

    lookup user during registration

--- wp-content/themes/enfold-child/js/gravity_forms/gform_1.js
+++ wp-content/themes/enfold-child/js/gravity_forms/gform_1.js
@@ -1,1 +1,29 @@
-console.log('gform_1 loaded');
+(function($) {
+  var $fName = $('#input_1_4_3')
+    , $lName = $('#input_1_4_6')
+    , $name = $fName.add($lName)
+    , $world = $('#input_1_5')
+    , $all = $name.add($world);
+
+  function searchPlayer() {
+    var fName = $fName.val()
+      , lName = $lName.val()
+      , world = $world.val()
+      , data = {
+        action: 'cw_searchCharacter',
+        first_name: fname,
+        last_name: lname,
+        server: world
+      };
+
+    $.post(ajaxurl, data)
+      .done(function(response) {
+        console.log(response);
+      })
+      .fail(function(response) {
+        console.error(response);
+      })
+  }
+
+  $name.blur()
+})(jQuery);
