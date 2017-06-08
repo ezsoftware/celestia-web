16261d7dd40d811ca52f88dfb1a905d2fabc9ba3
John Ryan <john@ezsoftwareinc.com>
2017-06-07 17:08:06 -0700

    fix if logic for when no character exists versus a character found but not exactly

--- wp-content/themes/enfold-child/js/gravity_forms/gform_1.js
+++ wp-content/themes/enfold-child/js/gravity_forms/gform_1.js
@@ -26,77 +26,69 @@
     $.post(ajaxurl, data)
       .done(function(response) {
         var s_data = JSON.parse(response);
-        if(s_data.length == 0) {
-          $found.val('false');
+        var bFound = false;
+        for(var i = 0; i < s_data.length; i++) {
+          var p_data = s_data[i];
+          if(p_data.name == (fname + " " + lname)) {  
+            bFound = true;            
+            $.post(ajaxurl, {
+              action: 'cw_getCharacterProfile',
+              character_id: p_data.id
+            }).done(function(char_data_response) {
+              var profile = JSON.parse(char_data_response)
+                , playerContainer = document.createElement("li")
+                , playerImage = document.createElement('img')
+                , playerName = document.createElement('span')
+                , playerTitle = document.createElement('span')
+                , playerWorld = document.createElement('span')
+                , playerFC = document.createElement('span')
+                , profileData = document.createElement('div');
+
+              playerName.className = 'player-name';
+              playerTitle.className = 'player-title';
+              playerWorld.className = 'player-world';
+              playerFC.className = 'player-fc';
+              profileData.className = "player-classes";
+
+              playerContainer.className = "player-box";
+              playerImage.src = "/image_proxy.php?url=" + encodeURIComponent(p_data.face);
+              playerName.innerHTML = p_data.name;
+              playerTitle.innerHTML = "&lt;&#8226; " + profile.title + " &#8226;&gt;";
+              playerWorld.innerHTML = p_data.world;
+              playerFC.innerHTML = p_data.free_company;
+
+              playerContainer.appendChild(playerImage);
+              playerContainer.appendChild(playerName);
+              playerContainer.appendChild(playerTitle);
+              playerContainer.appendChild(playerWorld);
+              playerContainer.appendChild(playerFC);                
+
+              for(var key in profile.classes) {
+                var classContainer = document.createElement('span')
+                  , className = document.createElement('span')
+                  , classLevel = document.createElement('span');
+                classContainer.className = 'job-class';
+                className.innerHTML = key;
+                classLevel.innerHTML = profile.classes[key];
+                classContainer.appendChild(className);
+                classContainer.appendChild(classLevel);
+                profileData.appendChild(classContainer);
+              }
+              playerContainer.appendChild(profileData);
+              $(playerContainer).insertAfter($('li#field_1_4'));
+
+              $('#input_1_6').val(p_data.id);
+              $('#input_1_8').val(char_data_response)
+              $('.overlay').remove();
+            });
+          }
+        }
+        if(!bFound) {
+          var playerContainer = document.createElement("li");
+          playerContainer.className = 'player-box';
+          playerContainer.innerHTML = "<span>Player Character Not Found... Please try again.</span>";
+          $(playerContainer).insertAfter($('li#field_1_4'));
           $('.overlay').remove();
-        } else {
-          var bFound = false;
-          for(var i = 0; i < s_data.length; i++) {
-            var p_data = s_data[i];
-            if(p_data.name == (fname + " " + lname)) {  
-              bFound = true;            
-              $.post(ajaxurl, {
-                action: 'cw_getCharacterProfile',
-                character_id: p_data.id
-              }).done(function(char_data_response) {
-                var profile = JSON.parse(char_data_response)
-                  , playerContainer = document.createElement("li")
-                  , playerImage = document.createElement('img')
-                  , playerName = document.createElement('span')
-                  , playerTitle = document.createElement('span')
-                  , playerWorld = document.createElement('span')
-                  , playerFC = document.createElement('span')
-                  , profileData = document.createElement('div');
-
-                playerName.className = 'player-name';
-                playerTitle.className = 'player-title';
-                playerWorld.className = 'player-world';
-                playerFC.className = 'player-fc';
-                profileData.className = "player-classes";
-
-                playerContainer.className = "player-box";
-                playerImage.src = "/image_proxy.php?url=" + encodeURIComponent(p_data.face);
-                playerName.innerHTML = p_data.name;
-                playerTitle.innerHTML = "&lt;&#8226; " + profile.title + " &#8226;&gt;";
-                playerWorld.innerHTML = p_data.world;
-                playerFC.innerHTML = p_data.free_company;
-
-                playerContainer.appendChild(playerImage);
-                playerContainer.appendChild(playerName);
-                playerContainer.appendChild(playerTitle);
-                playerContainer.appendChild(playerWorld);
-                playerContainer.appendChild(playerFC);                
-
-                for(var key in profile.classes) {
-                  var classContainer = document.createElement('span')
-                    , className = document.createElement('span')
-                    , classLevel = document.createElement('span');
-                  classContainer.className = 'job-class';
-                  className.innerHTML = key;
-                  classLevel.innerHTML = profile.classes[key];
-                  classContainer.appendChild(className);
-                  classContainer.appendChild(classLevel);
-                  profileData.appendChild(classContainer);
-                }
-                playerContainer.appendChild(profileData);
-                $(playerContainer).insertAfter($('li#field_1_4'));
-
-                $('#input_1_6').val(p_data.id);
-                $('#input_1_8').val(char_data_response)
-                $('.overlay').remove();
-              });
-            }
-          }
-          console.log(bfound);
-          if(!bFound) {
-            console.log('here');
-            var playerContainer = document.createElement("li");
-            playerContainer.className = 'player-box';
-            playerContainer.innerHTML = "<span>Player Character Not Found... Please try again.</span>";
-            $(playerContainer).insertAfter($('li#field_1_4'));
-            console.log(playerContainer);
-            $('.overlay').remove();
-          }
         }
       })
       .fail(function(response) {
