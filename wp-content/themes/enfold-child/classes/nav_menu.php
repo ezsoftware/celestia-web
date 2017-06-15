d7b8fb121b5393c49bedb57da139429bb7cf5665
John Ryan <john@ezsoftwareinc.com>
2017-06-15 16:10:38 -0700

    integrate Front_End_PM for the menu items "{unread_messages}" count.

--- wp-content/themes/enfold-child/classes/nav_menu.php
+++ wp-content/themes/enfold-child/classes/nav_menu.php
@@ -1,8 +1,13 @@
 <?php
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+if(file_exists(ABSPATH . '/wp-content/plugins/front-end-pm/includes/class-fep-message.php')) {
+  require_once ABSPATH . '/wp-content/plugins/front-end-pm/includes/class-fep-message.php';
+}
+
 class CW_NavMenu {  
   static $instance = null;
+  static $fep_message = null;
   public static function getInstance() {
     if(self::$instance === null) {
       self::$instance = new CW_NavMenu();
@@ -11,15 +16,20 @@ class CW_NavMenu {
   }
   private function __construct() {
     add_filter( 'wp_nav_menu_items', array($this, 'cw_nav_item_dynamics'), 10, 2);
+    if(class_exists('Fep_Message')) {
+      self::$fep_message = Fep_Message::init();
+    }
   }
 
   public function cw_nav_item_dynamics($items, $args) {
     $user = wp_get_current_user();
     $unread_count = 0;
+    if(self::$fep_message !== null) {
+      $unread_counts = self::$fep_message->user_message_count('all', false, $user->ID);
+      $unread_count = $unread_counts['unread'];
+    }
     foreach($user->data as $key => $value) {
-      if($key === '_fep_user_message_count') {
-        $unread_count += $value['unread'];
-      } else if(!is_object($value) && !is_array($value)) {
+      if(!is_object($value) && !is_array($value)) {
         $items = str_replace('{' . $key . '}', $value, $items);
       }
     }
@@ -32,7 +42,7 @@ class CW_NavMenu {
     $items = str_replace('{firstname}', $name[0], $items);
     $items = str_replace('{lastname}', $name[1], $items);
 	  $items = str_replace('{avatar}', get_avatar($user->ID, 16), $items);
-    $items = str_replace('{undread_messages}', $unread_count > 0 ? '<a href="/private-messages/" class="menu-unread-count">' . $unread_count . '</a>' : '', $items);
+    $items = str_replace('{unread_messages}', ($unread_count > 0 ? ('<span data-href="/private-messages/" class="menu-unread-count">' . $unread_count . '</span>') : ''), $items);
     return $items;
   }
 }
